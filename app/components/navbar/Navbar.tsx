70f7ba9fdc8dcd1e5c3163017b1dcf0c41159711
nihalgunu <nihalgunukula@gmail.com>
2024-07-10 20:27:50 -0700

    updated organization

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,47 @@
+const Navbar = () => {
+    return (
+        <div className="navbar bg-slate-300">
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-5 w-5 stroke-current">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"></path>
+            </svg>
+          </button>
+        </div>
+        <div className="flex-none">
+                  <a className="btn btn-ghost text-lg">Lendify</a>
+        </div>
+        <div className="flex-none">
+                  <a href= "/markets" className="btn btn-ghost text-lg">Markets</a>
+        </div>
+        <div className="flex-1">
+                  <a href="/governance" className="btn btn-ghost text-lg">Governance</a>
+        </div>
+        <div className="flex-none">
+          <button className="btn btn-square btn-ghost">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block h-5 w-5 stroke-current">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
+            </svg>
+          </button>
+        </div>
+      </div>
+    );
+}
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,85 +1,15 @@
 import Image from "next/image";
 import Link from "next/link";
+import Register from "./components/login/Register";
+import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
   return (
     <main>
-      <div className="navbar bg-slate-300">
-  <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        className="inline-block h-5 w-5 stroke-current">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M4 6h16M4 12h16M4 18h16"></path>
-      </svg>
-    </button>
-  </div>
-  <div className="flex-none">
-            <a className="btn btn-ghost text-lg">Lendify</a>
-  </div>
-  <div className="flex-none">
-            <a href= "/markets" className="btn btn-ghost text-lg">Markets</a>
-  </div>
-  <div className="flex-1">
-            <a href="/governance" className="btn btn-ghost text-lg">Governance</a>
-  </div>
-  <div className="flex-none">
-    <button className="btn btn-square btn-ghost">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        className="inline-block h-5 w-5 stroke-current">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path>
-      </svg>
-    </button>
-  </div>
-</div>
-
-<div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col items-center">
-    <div className="text-center mb-8">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shadow-2xl">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="text-center mt-4 flex justify-center w-full">
-        <p className="text-lg">Don&apos;t have an account?<a href="#" className="link link-primary">Sign up</a></p>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-
+      
+    <Navbar />
+    
+    <Register />
       
     </main>
   )
