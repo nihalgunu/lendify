465856da52fc92d648bfaf2ed8554596d2cec96b
nihalgunu <nihalgunukula@gmail.com>
2024-07-10 20:29:25 -0700

    slight name change

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Register from "./components/login/Register";
+import LoginGUI from "./components/login/LoginGUI";
 import Navbar from "./components/navbar/Navbar";
 
 export default function Home() {
@@ -8,8 +8,8 @@ export default function Home() {
     <main>
       
     <Navbar />
-    
-    <Register />
+
+    <LoginGUI />
       
     </main>
   )
