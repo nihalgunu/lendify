47db71161514321ddc9f9faba9ac9454926dcfea
sid <hisidharth2014@gmail.com>
2024-07-17 16:46:50 -0700

    gov and other things commit

--- app/components/governance/Governance.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const govPage = () => {
-  return (
-    <div>govPage</div>
-  )
-}
-
-export default govPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import LoginGUI from "./components/login/LoginGUI";
-import Navbar from "./components/navbar/Navbar";
+import LoginGUI from "./login/LoginGUI";
+import Navbar from "./navbar/Navbar";
 
 export default function Home() {
   return (
